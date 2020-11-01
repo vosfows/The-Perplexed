30c4d09001f4b90c38bd0b0aa3714398b4f1dad0
vosfows <73765410+vosfows@users.noreply.github.com>
2020-11-01 10:58:40 +0800

    Update authorize.js

--- authorize.js
+++ authorize.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
   let clientId = '779ee1b8200d4c4e900ece34dfaa8254';
   // Use the following site to convert your regular url to the encoded version: 
   // https://www.url-encode-decode.com/
-  const redirectUri= 'https://https://vosfows.github.io/spotify';
+  const redirectUri= 'https://vosfows.github.io/spotify';
   let query = `client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
   window.location = `${authEndpoint}?${query}`;
 });
