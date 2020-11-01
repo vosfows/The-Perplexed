9003e261fe341f124b7e565eacc167aac90eda95
vosfows <73765410+vosfows@users.noreply.github.com>
2020-11-01 10:41:52 +0800

    Update authorize.js

--- authorize.js
+++ authorize.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-  const authEndpoint = 'https://accounts.spotify.com/authorize';
+function redirectToSpotifyAuthentication() {
+    const authEndpoint = 'https://accounts.spotify.com/authorize';
     const clientId = '779ee1b8200d4c4e900ece34dfaa8254';
     const redirectUri = `https://vosfows.github.io/spotify`;
     let query = `client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
     window.location = `${authEndpoint}?${query}`;
-});
+}
