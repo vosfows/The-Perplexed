f8c629f9dee53c27455ba3def1381f1af0b6eb1b
vosfows <73765410+vosfows@users.noreply.github.com>
2020-11-01 11:50:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const inputElement = document.querySelector("inputValue");
         let src_str = `https://open.spotify.com/embed/track/${id}`;
         console.log(`src_str ${src_str}`);
         let iframe = `<iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
-        let parent_div = $('#content');
+        let parent_div = $('#music');
         parent_div.append(iframe);
       }
   });
