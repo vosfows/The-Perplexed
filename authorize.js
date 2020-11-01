4c4ca3a0ba9c5612327632c15280265c5ac5c8cf
vosfows <73765410+vosfows@users.noreply.github.com>
2020-11-01 11:01:43 +0800

    Update authorize.js

--- authorize.js
+++ authorize.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
   const authEndpoint = 'https://accounts.spotify.com/authorize';
-  let clientId = '779ee1b8200d4c4e900ece34dfaa8254';
+  const clientId = '779ee1b8200d4c4e900ece34dfaa8254';
   // Use the following site to convert your regular url to the encoded version: 
   // https://www.url-encode-decode.com/
   const redirectUri= 'https://vosfows.github.io/spotify';
