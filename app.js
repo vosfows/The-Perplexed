b2fca806f7ed90e48bdfb785e8be52781367f1cb
vosfows <73765410+vosfows@users.noreply.github.com>
2020-11-01 11:40:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,64 +1,59 @@
-var tools = require('spotify-web-api.js');
+$( document ).ready(function() {
+    console.log( 'script.js ready!' );
+    console.log('version 6')
 
-var request = require('request'); // "Request" library
-var http = require("http");
-var Spotify = require('spotify-web-api.js');
-var s = new Spotify();
+    const getUrlParameter = (sParam) => {
+      let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
+          sURLVariables = sPageURL.split('#'),
+          sParameterName,
+          i;
+      let split_str = window.location.href.split('#');
+      sURLVariables = split_str[1].split('&');
+      for (i = 0; i < sURLVariables.length; i++) {
+          sParameterName = sURLVariables[i].split('=');
+          if (sParameterName[0] === sParam) {
+              return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+          }
+      }
+  };
 
-
+    const accessToken = getUrlParameter('access_token');
+    console.log(`accessToken ${accessToken}`);
 const buttonElement = document.querySelector("#search");
 const inputElement = document.querySelector("inputValue");
-
-var client_id = '9ff915fc57e646e9af5e89bb9d6df6c7'; // Your client id
-var client_secret = '80aed081f2194d25bcb07a2f1264432a'; // Your secret
-var search = 'asdf';
-var spotify_playlist = 'https://api.spotify.com/v1/playlists/' + search + '/tracks';
-
-// your application requests authorization
-
-var console_content = '';
-
-
-
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
-
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-
-    // use the access token to access the Spotify Web API
-    var token = body.access_token;
-    var options = {
-      url: 'https://api.spotify.com/v1/users/jmperezperez',
+    
+ buttonElement.onclick = function(event){
+  event.preventDefault();
+  const value = inputValue.value;
+  const url = 'https://api.spotify.com/v1/search?q=';
+  const newUrl = url + value +'&type=track';
+     
+      $.ajax ({
+      url: newUrl,
+      type: 'GET',
       headers: {
-        'Authorization': 'Bearer ' + token
+          'Authorization' : 'Bearer ' + accessToken
       },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-      console.log(body);
-    });
-  }
+      success: function(data) {
+        console.log(' ');
+        console.log(' ');
+        console.log('Got data back');
+        // Let's console what gets returned for our search
+        console.log(JSON.stringify(data));
+        // Example: Extract the id of the song from the data object
+        let id = data.tracks.items[0].id;
+        console.log(' ');
+        console.log(`id ${id}`); ////id 1TEL6MlSSVLSdhOSddidlJ
+        // Constructing a iframe to embed a song
+        let src_str = `https://open.spotify.com/embed/track/${id}`;
+        console.log(`src_str ${src_str}`);
+        let iframe = `<iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
+        let parent_div = $('#content');
+        parent_div.append(iframe);
+      }
+  });
 
-  
-spotifyApi.setAccessToken(token);
-spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function (err, data) {
-  if (err) console.error(err);
-  else console.log('Artist albums', data);
+}
+    
+ 
 });
-
-
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.write(console_content);
-    response.end();
-  }).listen(8888);
