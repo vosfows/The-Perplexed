a317ce4adad9b05fafd16aa53dc6ab30d4b854bb
vosfows <73765410+vosfows@users.noreply.github.com>
2020-11-01 10:54:09 +0800

    Update authorize.js

--- authorize.js
+++ authorize.js
@@ -1,7 +1,9 @@
-function redirectToSpotifyAuthentication() {
-    const authEndpoint = 'https://accounts.spotify.com/authorize';
-    const clientId = '779ee1b8200d4c4e900ece34dfaa8254';
-    const redirectUri = `https://vosfows.github.io/spotify`;
-    let query = `client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
-    window.location = `${authEndpoint}?${query}`;
-}
+$( document ).ready(function() {
+  const authEndpoint = 'https://accounts.spotify.com/authorize';
+  let client_id = '779ee1b8200d4c4e900ece34dfaa8254';
+  // Use the following site to convert your regular url to the encoded version: 
+  // https://www.url-encode-decode.com/
+  const redirect_uri = 'https://https://vosfows.github.io/spotify';
+  let query = `client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
+  window.location = `${authEndpoint}?${query}`;
+});
