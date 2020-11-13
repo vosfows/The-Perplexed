8b983f9b951f27403da66d4de7c9745448c1a370
vosfows <73765410+vosfows@users.noreply.github.com>
2020-11-13 11:37:58 +0800

    Update app.js
    balik ajax

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+
 $( document ).ready(function() {
     console.log( 'script.js ready!' );
     console.log('version 6')
-    
 
     const getUrlParameter = (sParam) => {
       let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
@@ -24,23 +25,29 @@ $( document ).ready(function() {
     const inputElement = document.querySelector("inputValue");
     var music = ``
     
-  buttonElement.onclick = function(event){
+  $("form").keypress(function(e) {
+    if (e.which == 13) {
+        return false;
+    }
+    });
+ buttonElement.onclick = function(event){
   event.preventDefault();
   const value = inputValue.value;
   const url = 'https://api.spotify.com/v1/search?q=';
   const newUrl = url + value +'&type=track' + '&limit=10&offset=5';
+     
 
-  $.get({
-  	url:newUrl,
-  	type:"GET",
-  	headers:{
-  		"Authorization":"Bearer" + accessToken
-  	},
-  	 success: function(response) {
-       $("#music").empty();
+      $.ajax ({
+      url: newUrl,
+      type: 'GET',
+      headers: {
+          'Authorization' : 'Bearer ' + accessToken
+      },
+      success: function(response) {
+        $("#music").empty();
 
-       var results = response.tracks;
-       results.items.forEach(item => {
+        var results = response.tracks;
+        results.items.forEach(item => {
           music = `
 
           <iframe class="music" width="300" height="350" src="https://open.spotify.com/embed/track/${item.id}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
@@ -52,7 +59,6 @@ $( document ).ready(function() {
 
 
       }
-  })
-
+  });
 }
 });
