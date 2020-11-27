15fe68d342e80eb9accaeec3cd3413ecf4e0a81c
vosfows <73765410+vosfows@users.noreply.github.com>
2020-11-27 21:58:22 +0800

    Update authorize.js

--- js/authorize.js
+++ js/authorize.js
@@ -6,7 +6,7 @@ var ready = (callback) => {
 ready(() => {
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 const clientId = '779ee1b8200d4c4e900ece34dfaa8254';
-const redirectUri= 'https://vosfows.github.io/spotify/music.html';
+const redirectUri= 'https://vosfows.github.io/The-Perplexed/music.html';
 
 let query = 'client_id=' + clientId + '&redirect_uri=' + redirectUri + '&response_type=token&show_dialogue=true';
 window.location = authEndpoint + '?' + query;
